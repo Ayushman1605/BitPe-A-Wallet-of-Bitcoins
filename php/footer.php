8159beb7d43800af769eba968ac1d744eb62e52f
Ayushman1605 <85343082+Ayushman1605@users.noreply.github.com>
2021-09-03 20:27:24 +0530

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,4 @@
+  <?php echo "
+  <footer class='text-center mt-5 py-2'><b>&copy; AYUSHMAN TIWARI <script>
+        document.write(new Date().getFullYear())
+      </script>. All Rights Reserved</b></footer>" ?>
