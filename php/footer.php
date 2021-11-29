d3d04b9a05f43073f6eb8e0aece8f2aed25f776c
Ayushman1605 <85343082+Ayushman1605@users.noreply.github.com>
2021-11-29 23:32:12 +0530

    Delete php directory

--- php/footer.php
+++
@@ -1,4 +0,0 @@
-  <?php echo "
-  <footer class='text-center mt-5 py-2'><b>&copy; AYUSHMAN TIWARI <script>
-        document.write(new Date().getFullYear())
-      </script>. All Rights Reserved</b></footer>" ?>
